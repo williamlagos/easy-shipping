a2bcde231672c80f53cc9e353bd209a817d14548
William Oliveira de Lagos <william.lagos@live.com>
2017-11-07 16:16:19 -0200

    Fixed production settings

--- boxer/wsgi.py
+++ boxer/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kombi.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "boxer.settings")
 
 application = get_wsgi_application()
